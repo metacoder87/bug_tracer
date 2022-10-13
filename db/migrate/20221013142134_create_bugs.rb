e54df6f3282a469b11060713dbcba7b70f1dec3c
metacoder87 <gary.miller@gmx.com>
2022-10-13 10:58:40 -0400

    Adds index and default to bugs

--- db/migrate/20221013142134_create_bugs.rb
+++ db/migrate/20221013142134_create_bugs.rb
@@ -1,11 +1,13 @@
 class CreateBugs < ActiveRecord::Migration[6.1]
   def change
     create_table :bugs do |t|
-      t.text :path
-      t.text :description
-      t.boolean :status
+      t.text :path, null: false 
+      t.text :description, null: false
+      # True = Active unsolved bug / False = Archived resolved bug
+      t.boolean :status, :default => false 
 
       t.timestamps
     end
+    add_index :bugs, :path, unique: true 
   end
 end
