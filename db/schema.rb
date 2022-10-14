2ef4a198078fb9665e4db28f33277b946ec826b8
metacoder87 <gary.miller@gmx.com>
2022-10-14 15:54:12 -0400

    Adds user reference to bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_142134) do
+ActiveRecord::Schema.define(version: 2022_10_14_195052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,23 @@ ActiveRecord::Schema.define(version: 2022_10_13_142134) do
     t.boolean "status", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["path"], name: "index_bugs_on_path", unique: true
+    t.index ["user_id"], name: "index_bugs_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bugs", "users"
 end
