8d07ee0432af58f6f627c2be3d5807d2214fc529
metacoder87 <gary.miller@gmx.com>
2022-10-15 04:39:48 -0400

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'bugs#index'
   devise_for :users
-  resources :bugs
+
+  resources :bugs do
+    collection do
+      get :active
+      get :archive
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
