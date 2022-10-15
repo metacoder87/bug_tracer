7496dbdcd175b7b027adcaecc328bfc87b9290a6
metacoder87 <gary.miller@gmx.com>
2022-10-15 04:40:02 -0400

    Updates the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # protect_from_forgery with: :exception
+
+    # def current_user
+    #     return nil unless session[:session_token]
+    #     @current_user ||= User.find_by(session_token: session[:session_token])
+    # end
+    
+    # def require_user!
+    #     redirect_to new_session_url if current_user.nil?
+    # end
+
 end
