c686dfd6208546a2a5934b1e216c4a37bf5cd90e
metacoder87 <gary.miller@gmx.com>
2022-10-23 03:43:46 -0400

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_17_044821) do
+ActiveRecord::Schema.define(version: 2022_10_21_061114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,20 +32,10 @@ ActiveRecord::Schema.define(version: 2022_10_17_044821) do
     t.bigint "bug_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_comments_on_ancestry"
     t.index ["bug_id"], name: "index_comments_on_bug_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "replies", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id", null: false
-    t.bigint "bug_id", null: false
-    t.bigint "comment_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["bug_id"], name: "index_replies_on_bug_id"
-    t.index ["comment_id"], name: "index_replies_on_comment_id"
-    t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,7 +54,4 @@ ActiveRecord::Schema.define(version: 2022_10_17_044821) do
   add_foreign_key "bugs", "users"
   add_foreign_key "comments", "bugs"
   add_foreign_key "comments", "users"
-  add_foreign_key "replies", "bugs"
-  add_foreign_key "replies", "comments"
-  add_foreign_key "replies", "users"
 end
