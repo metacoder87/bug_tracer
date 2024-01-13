33bed15034f3cb402b861beb005bf23d41635a83
metacoder87 <gary.miller@gmx.com>
2024-01-13 13:50:09 -0500

    Updates to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :bugs do
-    resources :comments
+    resources :comments do
+      resources :replies
+    end
     collection do
       get :active
       get :archive
+      get :processing
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
