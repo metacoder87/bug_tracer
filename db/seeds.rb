143c9d96f285178cefa7aeec91bcf9cf50dbb1f6
metacoder87 <gary.miller@gmx.com>
2024-01-13 13:50:32 -0500

    Updates to the seeds app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeds.rb
+
+
+User.destroy_all
+Bug.destroy_all
+Comment.destroy_all
