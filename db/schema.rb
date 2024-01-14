432d15cd6a51e58392cc7d948946f06f563aa212
metacoder87 <gary.miller@gmx.com>
2024-01-14 02:16:07 -0500

    Updates the schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2024_01_06_000337) do
     t.text "description", null: false
     t.text "finder", null: false
     t.integer "status", default: 0, null: false
-    t.text "fixer"
+    t.text "fixer", default: "Unassigned"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
@@ -55,13 +55,18 @@ ActiveRecord::Schema.define(version: 2024_01_06_000337) do
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false
+    t.string "unconfirmed_email"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
