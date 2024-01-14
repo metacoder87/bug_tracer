c431cfbed312f925c93717690a1e8c62e56f728f
metacoder87 <gary.miller@gmx.com>
2024-01-14 02:16:52 -0500

    Updates dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,21 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'gmail.com',
+    user_name:       'bugtracer99@gmail.com',
+    password:        'fiix ckmt nnya zchn',
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    30,
+    read_timeout:    30 }
+
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -34,7 +49,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
