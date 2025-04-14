770606cee64fcb2716f82961336fe966fa673363
metacoder87 <gary.miller@gmx.com>
2025-04-14 10:36:42 -0400

    Adds in some seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,73 @@
 # seeds.rb
 
 
+require 'faker'
+
+# Clear existing data
 User.destroy_all
 Bug.destroy_all
 Comment.destroy_all
+Reply.destroy_all
+
+# Seed Users
+users = []
+10.times do
+  users << User.create!(
+    admin: Faker::Boolean.boolean,
+    unconfirmed_email: Faker::Internet.email,
+    email: Faker::Internet.email,
+    password: 'passWord1234',  # Use a common password for seeding
+    password_confirmation: 'passWord1234',
+    reset_password_token: Faker::Internet.uuid,
+    reset_password_sent_at: Faker::Time.backward(days: 365),
+    remember_created_at: Faker::Time.backward(days: 365),
+    confirmation_token: Faker::Internet.uuid,
+    confirmed_at: Faker::Time.backward(days: 365),
+    confirmation_sent_at: Faker::Time.backward(days: 365),
+    created_at: Faker::Time.backward(days: 365),
+    updated_at: Faker::Time.backward(days: 365)
+  )
+end
+
+# Seed Bugs
+bugs = []
+20.times do
+  bugs << Bug.create!(
+    project: Faker::App.name,
+    path: Faker::File.file_name(dir: 'projects'),
+    priority: rand(0..5),
+    description: Faker::Lorem.sentence,
+    finder: Faker::Name.name,
+    status: rand(0..3),
+    fixer: ['Unassigned', Faker::Name.name].sample,
+    created_at: Faker::Time.backward(days: 365),
+    updated_at: Faker::Time.backward(days: 365),
+    user_id: users.sample.id
+  )
+end
+
+# Seed Comments
+comments = []
+30.times do
+  comments << Comment.create!(
+    body: Faker::Lorem.paragraph,
+    user_id: users.sample.id,
+    bug_id: bugs.sample.id,
+    created_at: Faker::Time.backward(days: 365),
+    updated_at: Faker::Time.backward(days: 365)
+  )
+end
+
+# Seed Replies
+40.times do
+  Reply.create!(
+    body: Faker::Lorem.paragraph,
+    comment_id: comments.sample.id,
+    user_id: users.sample.id,
+    bug_id: bugs.sample.id,
+    created_at: Faker::Time.backward(days: 365),
+    updated_at: Faker::Time.backward(days: 365)
+  )
+end
+
+puts "Database successfully seeded!"
